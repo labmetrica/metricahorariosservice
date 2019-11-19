72632b73842ae717d8dc8ed7d947a90b40a89012
artfern95 <36283497+artfern95@users.noreply.github.com>
2019-11-19 09:29:46 +0100

    arreglados algunos errores

--- src/main/java/com/metrica/formacion/metricahorariosservice/Controller/HorariosDashboard.java
+++ src/main/java/com/metrica/formacion/metricahorariosservice/Controller/HorariosDashboard.java
@@ -13,14 +13,7 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.metrica.formacion.metricahorariosservice.dto.gruposDTO;
 import com.metrica.formacion.metricahorariosservice.dto.usuariosDTO;
