823da30cdb6e2b404ab215fcf2140cfbf82acca7
artfern95 <36283497+artfern95@users.noreply.github.com>
2019-11-27 09:46:54 +0100

    control de excepciones error 5xx y 4xx

--- src/main/java/com/metrica/formacion/metricahorariosservice/exceptionHandler/CustomErrorResponse.java
+++ src/main/java/com/metrica/formacion/metricahorariosservice/exceptionHandler/CustomErrorResponse.java
@@ -1,26 +1,13 @@
 package com.metrica.formacion.metricahorariosservice.exceptionHandler;
 
 import lombok.Data;
-<<<<<<< HEAD
-=======
-import lombok.NoArgsConstructor;
->>>>>>> origin/controller
 import org.springframework.http.HttpStatus;
 
 @Data
 public class CustomErrorResponse extends RuntimeException {
 
-<<<<<<< HEAD
 	private String exMessage;
 	private HttpStatus status;
-
-	public CustomErrorResponse(Class cla, String mensaje, String exmensaje, HttpStatus statusCode) {
-		super("Class name: " + cla.getSimpleName()+", "+mensaje);
-		this.exMessage = exmensaje;
-		status = statusCode;
-=======
-	private HttpStatus status;
-	private String exMessage;
 
 	public CustomErrorResponse(Class cla, String mensaje, String exmensaje) {
 		super("Class name: " + cla.getSimpleName()+", "+mensaje);
@@ -31,6 +18,5 @@ public class CustomErrorResponse extends RuntimeException {
 		super("Class name: " + cla.getSimpleName()+", "+mensaje);
 		this.exMessage = exmensaje;
 		this.status = status;
->>>>>>> origin/controller
 	}
 }
